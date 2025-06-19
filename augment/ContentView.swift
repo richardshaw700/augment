13c11abdd12cf8621748fa8b51bc49493ff63f0d
Richard <shaw.richard@icloud.com>
2025-06-19 12:05:00 -0400

    Initial Commit

---
+++ augment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  augment
+//
+//  Created by Richard Shaw on 2025-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ augmentTests/augmentTests.swift
@@ -0,0 +1,17 @@
+//
+//  augmentTests.swift
+//  augmentTests
+//
+//  Created by Richard Shaw on 2025-06-19.
+//
+
+import Testing
+@testable import augment
+
+struct augmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
